1285c0ba664612ea950f517e1e858f579ea54285
Friedslick6 <friedslick6@gmail.com>
2023-06-04 10:09:16 +1000

    Fancier yet Broken

--- Build-A-Pico/script.js
+++ Build-A-Pico/script.js
@@ -1,5 +1,15 @@
-var label = document.getElementsByClassName('label');
-console.log(window.getComputedStyle(label).getPropertyValue('--perceived-lightness'));
-label.textContent = label.textContent.split('').map((v) =>
-  Math.round(Math.random()) ? v.toUpperCase() : v.toLowerCase()
-).join('');
+var n = 0;
+setInterval(function() {
+	n += 1;
+	if (n % 20) document.querySelector(".sidepanel").style.setProperty('--background-position-y', n + "px");
+	if (n % 250) {
+		document.querySelectorAll(".title").forEach(title => {
+			title.textContent = title.textContent.toLowerCase().split('').map((c) =>
+				Math.random() < .5 ? c : c.toUpperCase()
+			).join('');
+		});
+		document.querySelectorAll(".subtitle").forEach(subtitle => {
+			subtitle.style.fontFamily = (subtitle.style.fontFamily == "Scribbles No_ligatures") ? "Scribbles Variant No_ligatures" : "Scribbles No_ligatures";
+		});
+	}
+})
