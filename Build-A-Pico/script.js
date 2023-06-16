d2fcd71c7e8cb15eb0197f36cd88092e75e3f487
Friedslick6 <friedslick6@gmail.com>
2023-06-16 23:46:05 +1000

    Animation/Script Improvements

--- Build-A-Pico/script.js
+++ Build-A-Pico/script.js
@@ -1,14 +1,20 @@
-setInterval(function () {
-		document.querySelectorAll(".title").forEach((title) => {
-			//for each element with the "title" class,
-			title.textContent = title.textContent
-				.toLowerCase()
-				.split("")
-				.map(
-					(
-						c //replace that element's text content with an array made from each character in lowercase,
-					) => (Math.random() < 0.5 ? c : c.toUpperCase()) //where each index of that array is randomly assigned to uppercase,
-				)
-				.join(""); //and then that array is rejoined
+const titles = Array.from(document.querySelectorAll(".title"));
+let frameCount = 0;
+
+function animateTitles() {
+	frameCount++;
+
+	if (frameCount % 15 === 0) {
+		titles.forEach(title => {
+			let newContent = Array.from(title.textContent.toLowerCase(), char => {
+				return Math.random() < 0.5 ? char : char.toUpperCase();
+			}).join("");
+
+			title.textContent = newContent;
 		});
-},250);
+	}
+
+	requestAnimationFrame(animateTitles);
+}
+
+requestAnimationFrame(animateTitles);
