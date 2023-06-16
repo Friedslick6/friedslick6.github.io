c49502c6fc814a4a4c6010557317288989be75b4
Friedslick6 <friedslick6@gmail.com>
2023-06-17 09:33:36 +1000

    Final title animation script

--- Build-A-Pico/script.js
+++ Build-A-Pico/script.js
@@ -2,8 +2,15 @@ const titles = document.querySelectorAll(".title");
 let frameCount = 0;
 
 function animateTitles() {
-	++frameCount % 15 == 0 && titles.forEach(title => {
-		title.textContent = title.textContent.replace(/[a-z]/gi, match => .5 > Math.random() ? match.toLowerCase() : match.toUpperCase())
+	frameCount++ % 15 == 0 && titles.forEach(title => {
+		let text = title.textContent.toLowerCase(),
+			length = text.length,
+			randomizedCaseTextContent = [];
+		for (let i = 0; i < length; i++) {
+			let char = text[i];
+			randomizedCaseTextContent += Math.random() < .5 ? char : char.toUpperCase()
+		}
+		title.textContent = randomizedCaseTextContent
 	}), requestAnimationFrame(animateTitles)
 }
 requestAnimationFrame(animateTitles);
