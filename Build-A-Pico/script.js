04bbd6264c4e4df7d42506db877efc78dbd20ed6
Friedslick6 <friedslick6@gmail.com>
2023-06-17 08:20:33 +1000

    Can't stop optimising

--- Build-A-Pico/script.js
+++ Build-A-Pico/script.js
@@ -2,17 +2,8 @@ const titles = document.querySelectorAll(".title");
 let frameCount = 0;
 
 function animateTitles() {
-	frameCount++;
-
-	if (frameCount % 15 === 0) {
-		titles.forEach(title => {
-			title.textContent = title.textContent.replace(/[a-z]/gi, (match) => {
-				return Math.random() < 0.5 ? match.toLowerCase() : match.toUpperCase();
-			});
-		});
-	}
-
-	requestAnimationFrame(animateTitles);
+	++frameCount % 15 == 0 && titles.forEach(title => {
+		title.textContent = title.textContent.replace(/[a-z]/gi, match => .5 > Math.random() ? match.toLowerCase() : match.toUpperCase())
+	}), requestAnimationFrame(animateTitles)
 }
-
 requestAnimationFrame(animateTitles);
